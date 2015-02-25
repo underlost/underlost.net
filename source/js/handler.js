2d5213977397f26c8b97ed8e914dbcef0161706c
Tyler Rilling <underlost@gmail.com>
2015-02-24 20:16:56 -0800

    redesign in progress

--- source/js/handler.js
+++ source/js/handler.js
@@ -1,50 +1,137 @@
-/* config dom id (optional) + config particles params */
-particlesJS('particles-js', {
+/* DESKTOP */
+
+function pJS_desktop(){
+  particlesJS('particles-js', {
     particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: {
+        opacity: .25
+      },
+      size: 1,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
         color: '#fff',
-        shape: 'circle', // "circle", "edge" or "triangle"
-        opacity: .25,
-        size: 1,
-        size_random: true,
-        nb: 100,
-        line_linked: {
-            enable_auto: true,
-            distance: 110,
-            color: '#fff',
-            opacity: .25,
-            width: 1,
-            condensed_mode: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 600
-            }
-        },
-        anim: {
-            enable: true,
-            speed: 2
+        opacity: 0.2,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
         }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
     },
     interactivity: {
-        enable: true,
-        mouse: {
-            distance: 300
-        },
-        detect_on: 'canvas', // "canvas" or "window"
-        mode: 'grab',
-        line_linked: {
-            opacity: .15
-        },
-        events: {
-            onclick: {
-                enable: true,
-                mode: 'push', // "push" or "remove"
-                nb: 3
-            }
+      enable: true,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .2
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
         }
+      }
     },
-    /* Retina Display Support */
     retina_detect: true
-});
+  });
+}
+
+
+
+/* MOBILE / TABLET */
+
+function pJS_mobile(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: .25,
+      size: 1.5,
+      size_random: true,
+      nb: 40,
+      line_linked: {
+        enable_auto: false,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: false,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+/* LAUNCH */
+
+if(window.innerWidth > 1100){
+  pJS_desktop();
+}else{
+  pJS_mobile();
+}
+
+/* on resize */
+
+window.addEventListener('resize', function() {
+  checkOnResize();
+}, true);
+
+function checkOnResize(){
+  if(window.innerWidth > 1100){
+    if(pJS.particles.nb != 150){
+      console.log('desktop mode')
+      pJS.fn.vendors.destroy();
+      pJS_desktop();
+    }
+  }else{
+    if(pJS.particles.nb == 150){
+      console.log('mobile mode');
+      pJS.fn.vendors.destroy();
+      pJS_mobile();
+    }
+  }
+}
 
 /*
 * Add a CSS3 animation class to an element only when it's in the viewport.
