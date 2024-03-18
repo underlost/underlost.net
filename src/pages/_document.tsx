05a3cf1e8b6c103e55a2c92020cc7d1bc22efa47
Tyler Rilling <tyler@underlost.net>
2024-03-18 14:45:01 -0700

    Add GA tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 
 export default function Document() {
@@ -6,6 +7,24 @@ export default function Document() {
     <Html lang="en">
       <link rel="shortcut icon" href="/images/favicon.png" />
       <Head />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-YZ280RLRRY`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YZ280RLRRY', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <body className="min-h-screen">
         <Main />
         <NextScript />
