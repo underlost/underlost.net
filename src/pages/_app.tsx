86938a77ec10f84b5dff023d59fccc3115dfeb93
Tyler Rilling <tyler@underlost.net>
2025-02-14 10:46:40 -0800

    GA4 fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,68 @@
 import '@/styles/globals.scss'
 import '@/styles/prism.css'
 
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 import ThemeContextProvider from '../context/ThemeContext'
 import AccessibleContextProvider from '../context/AccessibleContext'
 import { HelmetProvider } from 'react-helmet-async'
 import { AnimatePresence } from 'framer-motion'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (typeof window.gtag === `function`) {
+        window.gtag(`config`, `G-YZ280RLRRY`, {
+          page_path: url,
+        })
+        console.log(`Page view tracked: ${url}`) // Optional: for debugging
+      }
+    }
+
+    // Listen to route changes and track page views
+    router.events.on(`routeChangeComplete`, handleRouteChange)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      router.events.off(`routeChangeComplete`, handleRouteChange)
+    }
+  }, [router.events])
+
+
   return (
-    <ThemeContextProvider>
-      <AccessibleContextProvider>
-        <HelmetProvider>
-          <AnimatePresence mode="wait">
-            <Component {...pageProps} />
-          </AnimatePresence>
-        </HelmetProvider>
-      </AccessibleContextProvider>
-    </ThemeContextProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-YZ280RLRRY`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YZ280RLRRY', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeContextProvider>
+        <AccessibleContextProvider>
+          <HelmetProvider>
+            <AnimatePresence mode="wait">
+              <Component {...pageProps} />
+            </AnimatePresence>
+          </HelmetProvider>
+        </AccessibleContextProvider>
+      </ThemeContextProvider>
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,23 +7,9 @@ export default function Document() {
     <Html lang="en">
       <link rel="shortcut icon" href="/images/favicon.png" />
       <Head>
-        <GoogleAnalytics gaId="G-YZ280RLRRY" />
+        
       </Head>
-      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-YZ280RLRRY`} />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-YZ280RLRRY', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <GoogleAnalytics gaId="G-YZ280RLRRY" />
       <body className="min-h-screen">
         <Main />
         <NextScript />
