e64b2a5c75ff4797c5e21137e197612435cb3326
Tyler Rilling <tyler@underlost.net>
2025-02-24 21:56:07 -0800

    update stripe api version

--- src/pages/api/stripe/webhook.ts
+++ src/pages/api/stripe/webhook.ts
@@ -31,7 +31,7 @@ if (
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(STRIPE_SECRET_KEY as string, {
-  apiVersion: `2025-01-27.acacia`, // Ensure the API version matches your dashboard settings
+  apiVersion: `2025-02-24.acacia`, // Ensure the API version matches your dashboard settings
 })
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
